13952fc3a8c32414ad7190aeca251d84267c7139
anarmh <anarmh@code.edu.az>
2023-08-04 17:56:04 +0400

    add to  wishlist from product detail

--- Gamehoax-backend/Controllers/ProductDetailController.cs
+++ Gamehoax-backend/Controllers/ProductDetailController.cs
@@ -1,21 +1,31 @@
 ﻿using Gamehoax_backend.Models;
 using Gamehoax_backend.Services.Interfaces;
 using Gamehoax_backend.Viewmodel;
+using Gamehoax_backend.Viewmodel.Wishlist;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Gamehoax_backend.Controllers
 {
     public class ProductDetailController : Controller
     {
         private readonly IProductService _productService;
+        private readonly IHttpContextAccessor _accessor;
 
-        public ProductDetailController(IProductService productService)
+        public ProductDetailController(IProductService productService, IHttpContextAccessor accessor)
         {
             _productService = productService;
+            _accessor = accessor;
         }
 
         public async Task<IActionResult> Index(int? id)
         {
+
+            if (_accessor.HttpContext.Request.Cookies["wishlist"] != null)
+            {
+                var myCookie = JsonConvert.DeserializeObject<List<WishlistVM>>(_accessor.HttpContext.Request.Cookies["wishlist"]);
+                ViewBag.Cookie = myCookie;
+            }
             try
             {
                 if (id is null) return BadRequest();
